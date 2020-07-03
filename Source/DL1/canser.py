6a4402268ee1b3127ff866de759b57de4ace58af
albattawi <66701671+albattawi@users.noreply.github.com>
2020-07-02 21:13:33 -0500

    Update canser.py

--- Source/DL1/canser.py
+++ Source/DL1/canser.py
@@ -6,19 +6,6 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 
-# dataset = pd.read_csv("Breas Cancer.csv", header=None).values
-
-# X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,0:8], dataset[:,8],
-#                                                     test_size=0.25, random_state=87)
-# np.random.seed(155)
-# my_first_nn = Sequential() # create model
-# my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
-# my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
-# my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-# my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100, initial_epoch=0)
-# print(my_first_nn.summary())
-# print(my_first_nn.evaluate(X_test, Y_test))
-#######################################################
 
 dataset = pd.read_csv("Breas Cancer.csv", header=1).values
 
