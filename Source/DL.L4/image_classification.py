ae62e2e930fc9bea29245f4cd89bf320eef80dd1
albattawi <66701671+albattawi@users.noreply.github.com>
2020-07-16 23:57:10 -0500

    Deep learning lesson 4

---
+++ Source/DL.L4/image_classification.py
@@ -0,0 +1,91 @@
+# Simple CNN model for CIFAR-10
+import numpy
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+# Use set_image_data_format() to be compatible with keras version: 2.4.3 and 'channel_last'
+#channels last: image will be represnted as 3D array and last channel will indecate the color.
+#channels First.: image will be represnted as 3D array and first channel will indecate the color.
+#K.set_image_dim_ordering('th')
+
+K.set_image_data_format('channels_last')
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# normalize inputs from 0-255 to 0.0-1.0
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+
+# 1- Build New Model with below requirments
+model = Sequential()
+# Con. input layer 32
+model.add(Conv2D(32, kernel_size=3, input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2,))
+model.add(Conv2D(32, kernel_size=3, activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Con. input layer 64
+model.add(Conv2D(64, kernel_size=3, input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Con. input layer 128
+model.add(Conv2D(128, kernel_size=3, input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, kernel_size=3, activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(10,  activation='softmax'))
+
+# Compile model
+epochs = 1
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+print(model.summary())
+
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+model.save('./model' + '.h5')
+predectlist = []
+predectlist = model.predict(X_test)
+import matplotlib.pyplot as plt
+for x in range(4):
+    print('Predect Img_Test',x,': ',predectlist[x])
+    plt.imshow(X_test[x],aspect='auto')
+    plt.show()
+    print('Actual label:',[numpy.argmax(y_test[x])])
+
+
+
+
+
+
+
